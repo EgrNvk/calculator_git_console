9e38f311068f5e8b8d5f91a265f8073d9ebfd2af
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-10-30 13:22:33 +0200

    Add function addition

--- calculator_git_console.py
+++ calculator_git_console.py
@@ -2,3 +2,8 @@ class Calculator:
     def __init__(self, number1, number2):
         self.number1 = number1
         self.number2 = number2
+
+    def addition(self):
+        result = self.number1 + self.number2
+        print(f"{self.number1} + {self.number2} = {result}")
+        return
